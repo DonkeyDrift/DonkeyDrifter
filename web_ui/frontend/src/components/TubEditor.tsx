fd34d1e44ae0ac1f8fbffc1163a6d3d8788cb4a9
Nowhere_X <haobot@foxmail.com>
2026-05-27 16:17:13 +0800

    fix(TubEditor): 调整图表Y轴范围和刻度步长

--- web_ui/frontend/src/components/TubEditor.tsx
+++ web_ui/frontend/src/components/TubEditor.tsx
@@ -1096,11 +1096,11 @@ export const TubEditor: React.FC = () => {
             grid: { color: '#27272a' }
         },
         y: {
-            min: -1,
-            max: 1,
+            min: -100,
+            max: 100,
             ticks: {
               color: '#71717a',
-              stepSize: 0.2,
+              stepSize: 20,
             },
             grid: { color: '#27272a' }
         }
